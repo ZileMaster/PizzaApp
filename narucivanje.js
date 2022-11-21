8fa0ab72d168c46f1d2a93f98f066f3af7cbce35
ZileMaster <azivkovic10120ri@raf.rs>
2022-11-21 17:39:43 +0100

    third commit

--- narucivanje.js
+++ narucivanje.js
@@ -11,4 +11,33 @@ function izaberi(radiobtn){
     }
 
     document.getElementById("pizza").innerHTML = tekst;
-}
+
+    }
+    window.addEventListener("load", function(){
+        this.document.getElementById("form").addEventListener("submit", e=>{
+            let validno = true;
+
+            if(this.document.getElementById("kolicina").value <= 0){
+                validno = false;
+            }
+            if(this.document.getElementById("vreme-dostave").value <= 0){
+                validno = false;
+            }
+            if(this.document.getElementById("phoneNum").value == ""){
+                validno = false;
+            }
+            if(this.document.getElementById("adresa").value == ""){
+                validno = false;
+            }
+
+            if(this.document.getElementById("pizza").innerHTML == "(chose)" || this.document.getElementById("pizza").innerHTML == ""){
+                validno = false;
+            }
+
+            if(!validno){
+                this.window.alert("Popunite sva polja!");
+                e.preventDefault();
+            }
+        });
+    });
+    
